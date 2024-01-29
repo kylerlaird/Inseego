f5d4584e048a1740a77559f6ae1baf14ec63cd98
Matt Parnell <mparnell@gmail.com>
2024-01-29 17:22:45 -0600

    Adjust/add auth for FX2000. I was lazy and had ChatGPT do most of the heavy lifting but it turned out alright.

--- restart.py
+++ restart.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import Inseego
+from Inseego import M3000
+#from Inseego import FX2000
 
 password = input('password: ')
 
-m = Inseego.M3000('192.168.1.1')
+m = M3000('192.168.1.1')
+#m = FX2000('192.168.1.1')
 m.authenticate(password)
 m.restart()
 print('\nrestarting...')
--- status.py
+++ status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import Inseego
+from Inseego import M3000
+#from Inseego import FX2000
 
 m = Inseego.M3000('192.168.1.1')
+#m = FX2000('192.168.1.1')
 
 status = m.status()
 print('connection: %(statusBarConnectionState)s' % status['statusData'])
