5d712896efad813c0b3bd01202276a4a1af47c2c
Kyler Laird <kyler@lairds.com>
2024-02-09 20:46:25 -0600

    Update imports to allow specifying access point hardware.

--- restart.py
+++ restart.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from Inseego import M3000
-#from Inseego import FX2000
+from Inseego import M3000 as AP
+#from Inseego import FX2000 as AP
+
+m = AP('192.168.1.1')
 
 password = input('password: ')
 
-m = M3000('192.168.1.1')
-#m = FX2000('192.168.1.1')
 m.authenticate(password)
 m.restart()
 print('\nrestarting...')
